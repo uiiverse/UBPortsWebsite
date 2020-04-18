f8de972c9491cdc5d0c618972900b99b46d617a9
meltstrap <meltstrap@uiiverse.xyz>
2020-04-18 13:38:41 -0400

    Added website files

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+<link href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+<link href="css/main.css" rel="stylesheet">
+    <title>Uiiverse for Nintendo Switch</title>
+</head>
+<body>
+    <h2>Uiiverse for UBPorts</h2>
+    <p>In our goal on making not only a community for Nintendo fans to hang out, but an ethical, non-profit social network we decided to port Uiiverse to an ethical platform. So we're happy to announce Uiiverse is now available to download for UBports. If you're not aware of what UBports is, you can always <a href="https://ubports.com/">check out their website</a>.</p>
+        <p><a href="https://open-store.io/app/uiiverse.meltstrap"><img src="https://open-store.io/badges/en_US.svg" alt="OpenStore" /></a> / <a href="https://github.com/uiiverse/UBPorts">Source Code</a></p>
+<a href="/">« Go back to Uiiverse</a>
+</body>
+</html>
