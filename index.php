025ffbfab7110823852b1251c43aa13265ea090b
meltstrap <meltstrap@uiiverse.xyz>
2020-04-18 13:46:16 -0400

    Changed website title and fixed OpenStore button

--- index.php
+++ index.php
@@ -6,12 +6,12 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
 <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
 <link href="css/main.css" rel="stylesheet">
-    <title>Uiiverse for Nintendo Switch</title>
+    <title>Uiiverse for UBports</title>
 </head>
 <body>
     <h2>Uiiverse for UBPorts</h2>
     <p>In our goal on making not only a community for Nintendo fans to hang out, but an ethical, non-profit social network we decided to port Uiiverse to an ethical platform. So we're happy to announce Uiiverse is now available to download for UBports. If you're not aware of what UBports is, you can always <a href="https://ubports.com/">check out their website</a>.</p>
-        <p><a href="https://open-store.io/app/uiiverse.meltstrap"><img src="https://open-store.io/badges/en_US.svg" alt="OpenStore" /></a> / <a href="https://github.com/uiiverse/UBPorts">Source Code</a></p>
+        <p><a href="https://open-store.io/app/uiiverse.meltstrap"><img src="https://open-store.io/badges/en_US.png" alt="OpenStore" /></a> / <a href="https://github.com/uiiverse/UBPorts">Source Code</a></p>
 <a href="/">« Go back to Uiiverse</a>
 </body>
 </html>
